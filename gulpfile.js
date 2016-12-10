37b94b9ea305aa2ce3b9c5b67236dec856184888
Sailesh Kasaju <leosailesh@gmail.com>
2016-12-10 10:16:06 +0545

    added README.md, gulpfile.js, .babelrc, .eslintrc.json, and initial project structure

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', function() {
+				// Run ESLint
+
+	// Node source
+	gulp.src("es6/**/*.js")
+				.pipe(babel())
+				.pipe(gulp.dest("dist"));
+	// browser source
+	gulp.src("public/es6/**/*.js")
+				.pipe(babel())
+				.pipe(gulp.dest("public/dist"));
+});
